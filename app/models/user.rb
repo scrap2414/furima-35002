8fb3e1971c8536f613799201fcec560f2fd8a8a5
scrap2414 <80100413+scrap2414@users.noreply.github.com>
2021-04-02 18:11:45 +0900

    入力画面のエラーを表示

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,15 +5,15 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
          validates :nickname, presence: true
-         validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i , message: '英語と数字の両方を含めて設定してください' }
-         validates :birthday, presence: true
+         validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i , message: 'は英語と数字の両方を含めて設定してください' }
+         validates :birthday, presence: true 
 
-         with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
+         with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'は全角文字を使用してください' } do
           validates :first_name
           validates :last_name
         end
 
-        with_options presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: 'カタカナを使用してください' } do
+        with_options presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: 'はカタカナを使用してください' } do
           validates :first_name_kana
           validates :last_name_kana
         end
