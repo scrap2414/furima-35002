d1b341359a949a5288a13d4ccb3bbad15719235c
scrap2414 <80100413+scrap2414@users.noreply.github.com>
2021-04-02 16:13:45 +0900

    新規登録の文字制約バリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,13 +5,22 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
          validates :nickname, presence: true
-         validates :last_name, presence: true
-         validates :first_name, presence: true
-         validates :last_name_kana, presence: true
-         validates :first_name_kana, presence: true
+         validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i , message: '英語と数字の両方を含めて設定してください' }
          validates :birthday, presence: true
+
+         with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
+          validates :first_name
+          validates :last_name
+        end
+
+        with_options presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: 'カタカナを使用してください' } do
+          validates :first_name_kana
+          validates :last_name_kana
+        end
 
          has_many  :items
          has_many  :others
 
  end 
+
+
