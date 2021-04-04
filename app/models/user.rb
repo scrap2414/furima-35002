8fb9d3f3413552e10ce601058f0fb778f906dda1
scrap2414 <80100413+scrap2414@users.noreply.github.com>
2021-04-04 16:33:24 +0900

    Rubocopを使用

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,23 +4,20 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         validates :nickname, presence: true
-         validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i , message: 'は英語と数字の両方を含めて設定してください' }
-         validates :birthday, presence: true 
+  validates :nickname, presence: true
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'は英語と数字の両方を含めて設定してください' }
+  validates :birthday, presence: true
 
-         with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'は全角文字を使用してください' } do
-          validates :first_name
-          validates :last_name
-        end
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'は全角文字を使用してください' } do
+    validates :first_name
+    validates :last_name
+  end
 
-        with_options presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: 'はカタカナを使用してください' } do
-          validates :first_name_kana
-          validates :last_name_kana
-        end
+  with_options presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: 'はカタカナを使用してください' } do
+    validates :first_name_kana
+    validates :last_name_kana
+  end
 
-         has_many  :items
-         has_many  :others
-
- end 
-
-
+  has_many  :items
+  has_many  :others
+end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,14 +1,13 @@
 FactoryBot.define do
   factory :user do
-    nickname              {Faker::Name.initials(number: 2)}
-    email                 {Faker::Internet.free_email}
-    password              {'XXXX5588'}
-    password_confirmation {password}
-    first_name            {'春'}          
-    last_name             {'風'}    
-    first_name_kana       {'ハル'}    
-    last_name_kana        {'カゼ'}    
-    birthday              {'2000-01-01'}    
+    nickname              { Faker::Name.initials(number: 2) }
+    email                 { Faker::Internet.free_email }
+    password              { 'XXXX5588' }
+    password_confirmation { password }
+    first_name            { '春' }
+    last_name             { '風' }
+    first_name_kana       { 'ハル' }
+    last_name_kana        { 'カゼ' }
+    birthday              { '2000-01-01' }
   end
 end
-
