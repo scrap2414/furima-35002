55a6b03ad33a6fdf60f50461fc3e7ff49069366c
scrap2414 <80100413+scrap2414@users.noreply.github.com>
2021-04-05 22:48:36 +0900

    配送元の地域プルダウンの表示

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,11 +2,11 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :status
+  belongs_to :prefecture
 
 
   validates :name, presence: true
   validates :information, presence: true
-  validates :prefecture_id, presence: true
   validates :delivery_price_id, presence: true
   validates :delivery_day_id, presence: true
   validates :item_price, presence: true
@@ -19,6 +19,7 @@ class Item < ApplicationRecord
   #ジャンルの選択が「--」の時は保存できないようにする
   validates :category_id, numericality: { other_than: 1 } 
   validates :status_id, numericality: { other_than: 1 } 
+  validates :prefecture_id, numericality: { other_than: 1 } 
 end
 
 
