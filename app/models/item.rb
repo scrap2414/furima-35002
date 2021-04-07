9d0580f7f13b722310605d95256941e2d52c940b
scrap2414 <80100413+scrap2414@users.noreply.github.com>
2021-04-07 15:08:44 +0900

    Revert "Revert "商品一覧表示機能の実装""
    This reverts commit f06e0f24d16f58ea54c34829e43f3d456615f431.

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,9 +1,8 @@
 class Item < ApplicationRecord
-  extend ActiveHash::Associations::ActiveRecordExtensions
-
   belongs_to :user
   has_one    :order
-  has_one_attached :image
+  has_one_attached :image, dependent: :destroy
+  extend ActiveHash::Associations::ActiveRecordExtensions
 
   belongs_to :category
   belongs_to :status
