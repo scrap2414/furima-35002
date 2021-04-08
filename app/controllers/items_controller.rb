5a7b4ddfef20a5458985f3c17ee5f10e2699fb15
scrap2414 <80100413+scrap2414@users.noreply.github.com>
2021-04-08 15:00:11 +0900

    Rubocop使用

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
-  before_action :move_to_index, only: [:edit,:destroy]
+  before_action :move_to_index, only: [:edit, :destroy]
 
   def index
     @items = Item.order('created_at DESC')
