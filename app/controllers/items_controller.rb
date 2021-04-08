a050747f51cad7012030a28e5fac85ab2fc224e4
scrap2414 <80100413+scrap2414@users.noreply.github.com>
2021-04-08 11:48:15 +0900

    エラーメッセージの表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -26,6 +26,12 @@ class ItemsController < ApplicationController
   end
 
   def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+       redirect_to root_path    
+    else 
+    　　render :edit
+    end
   end
 
   private
