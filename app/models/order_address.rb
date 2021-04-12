ab255ac3b30e4c797277c413109e38a42b8d6e5d
scrap2414 <80100413+scrap2414@users.noreply.github.com>
2021-04-12 17:03:31 +0900

    Order_Addressの記述

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,10 +1,19 @@
-class クラス名
+class OrderAddress
   include ActiveModel::Model
-  attr_accessor :hoge,:fuga...
+  attr_accessor :postal_code, :prefecture_id, :municipality, :house_number, :building, :phone_number, :user_id, :item_id
 
-  # ここにバリデーションの処理を書く
+  with_options presence: true do
+    validates :municipality
+    validates :house_number
+    validates :item_id
+    validates :user_id
+    validates :phone_number,format: { with: /\A[0-9]+\z/ }
+    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/}
+  end
+  validates :prefecture, numericality: {other_than: 1, message: "can't be blank"}
 
   def save
     # 各テーブルにデータを保存する処理を書く
   end
 end
+
