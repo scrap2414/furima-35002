04678dffd951a79dd6e8d8ab46d80b9198696ff7
scrap2414 <80100413+scrap2414@users.noreply.github.com>
2021-04-12 19:35:18 +0900

    購入ページのカラム記述

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,12 +2,12 @@ class OrdersController < ApplicationController
 
   def index
     @item = Item.find(params[:item_id])
+    @order_address = OrderAddress.new
   end
 
-  def new
-  end
 
   def create
+    binding.pry
   end
 
 
