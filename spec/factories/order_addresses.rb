a7d9e0fa5aa22353405394b62aef2bef7de6d421
scrap2414 <80100413+scrap2414@users.noreply.github.com>
2021-04-13 14:38:59 +0900

    テストコード（雛形）作成

--- spec/factories/order_addresses.rb
+++ spec/factories/order_addresses.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :order_address do
     postal_code { '123-4567' }
-    prefecture_id { 1 }
+    prefecture_id { 6 }
     municipality { '横浜市' }
     house_number { '1-1' }
     building { '春風ハイツ' }
--- spec/models/order_address_spec.rb
+++ spec/models/order_address_spec.rb
@@ -10,16 +10,23 @@ RSpec.describe OrderAddress, type: :model do
   describe '商品購入機能' do
     context '商品購入できるとき' do
       it '全ての入力項目が存在すれば登録できる' do
+        expect(@order_address).to be_valid
       end
-    end
-  
-
-
-  
+      it '建物名が存在しなくても登録できる' do
+        @order_address.building =''
+        expect(@order_address).to be_valid
+      end
+      it '電話番号が11文字以下でも登録できる' do
+        @order_address.phone_number ='090222111'
+        expect(@order_address).to be_valid
+      end
+    end  
     context '商品購入できないとき' do
       it '不備' do
+        @order_address.item_id =''
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Item can't be blank")
       end
     end
-    
   end
 end
