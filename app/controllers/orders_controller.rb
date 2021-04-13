645aef2beaed5fffa264522c7228763de34c1b80
scrap2414 <80100413+scrap2414@users.noreply.github.com>
2021-04-13 23:45:56 +0900

    クレジットカード入金確認

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -7,9 +7,9 @@ class OrdersController < ApplicationController
 
 
   def create
- 
     @order_address = OrderAddress.new(order_params)
     if  @order_address.valid?
+      pay_item
       @order_address.save
       redirect_to root_path
     else
@@ -25,6 +25,16 @@ class OrdersController < ApplicationController
   def order_params
     params.require(:order_address).permit(:postal_code, :prefecture_id, :municipality, 
       :house_number, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+  def pay_item
+    item = Item.find(params[:item_id])
+    Payjp.api_key = "sk_test_8b364a99642d0606dcb17395"  # 自身のPAY.JPテスト秘密鍵を記述しましょう
+    Payjp::Charge.create(
+      amount: item[:itemprice],  # 商品の値段
+      card: order_params[:token],    # カードトークン
+      currency: 'jpy'                 # 通貨の種類（日本円）
+    )
   end
 
 
--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -8,6 +8,7 @@ class OrderAddress
     validates :house_number
     validates :item_id
     validates :user_id
+    validates :token
     validates :phone_number,format: { with: /\A[0-9]+\z/ }
     validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/}
   end
