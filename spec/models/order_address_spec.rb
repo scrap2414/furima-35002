90d3db4b60de41d5ee86ee8eb4f1311e3fc83b85
scrap2414 <80100413+scrap2414@users.noreply.github.com>
2021-04-14 16:57:35 +0900

    商品購入機能のテストコード実行

--- spec/models/order_address_spec.rb
+++ spec/models/order_address_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe OrderAddress, type: :model do
     user = FactoryBot.create(:user)
     item = FactoryBot.create(:item)
     @order_address = FactoryBot.build(:order_address, user_id: user.id, item_id: item.id)
+    sleep 0.1 
   end
 
   describe '商品購入機能' do
@@ -21,11 +22,72 @@ RSpec.describe OrderAddress, type: :model do
         expect(@order_address).to be_valid
       end
     end  
+
     context '商品購入できないとき' do
-      it '不備' do
+      it '郵便番号が入力されていない場合購入できない' do
+        @order_address.postal_code =''
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Postal code can't be blank")
+      end
+      it '郵便番号にハイフン（ー）が入力されていない場合購入できない' do
+        @order_address.postal_code ='2230003'
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Postal code is invalid")
+      end
+      it '郵便番号が正しく入力されていない場合購入できない（英字）' do
+        @order_address.postal_code ='223-000ww'
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Postal code is invalid")
+      end
+      it '郵便番号が正しく入力されていない場合購入できない（全角）' do
+        @order_address.postal_code ='２３４−３３４４'
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Postal code is invalid")
+      end
+      it '都道府県が入力されていない場合購入できない' do
+        @order_address.prefecture_id ='1'
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Prefecture can't be blank")
+      end
+      it '市区町村が入力されていない場合購入できない' do
+        @order_address.municipality =''
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Municipality can't be blank")
+      end
+      it '番地が入力されていない場合購入できない' do
+        @order_address.house_number =''
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("House number can't be blank")
+      end
+      it '電話番号が入力されていない場合購入できない' do
+        @order_address.phone_number =''
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Phone number can't be blank")
+      end
+      it '電話番号が正しくされていない場合購入できない（英字）' do
+        @order_address.phone_number ='090000299uu'
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Phone number is invalid")
+      end
+      it '電話番号が正しくされていない場合購入できない（全角）' do
+        @order_address.phone_number ='０９０８８７７５５３３'
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Phone number is invalid")
+      end
+      it 'tokenが含まれていない場合購入できない' do
+        @order_address.token =''
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Token can't be blank")
+      end
+      it '商品情報が結びついていない場合購入できない' do
         @order_address.item_id =''
         @order_address.valid?
         expect(@order_address.errors.full_messages).to include("Item can't be blank")
+      end
+      it 'ユーザー情報が結びついていない場合購入できない' do
+        @order_address.user_id =''
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("User can't be blank")
       end
     end
   end
