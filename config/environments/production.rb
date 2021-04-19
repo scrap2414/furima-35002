879d4923ff8220a8f5c5c159e0434f03f849ae35
scrap2414 <80100413+scrap2414@users.noreply.github.com>
2021-04-19 16:14:45 +0900

    修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -111,4 +111,3 @@ Rails.application.configure do
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 end
 
-server '18.219.36.88', user: 'ec2-user', roles: %w{app db web}
